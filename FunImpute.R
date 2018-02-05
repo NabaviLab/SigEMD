f69817f980f2ead9e842d8ea67542bb8961d30b8
tianyu-github <tianyu.wang@uconn.edu>
2018-02-05 15:38:52 -0500

    Add files via upload
    Impute function

---
+++ FunImpute.R
@@ -0,0 +1,94 @@
+FunImpute <- function(
+  object,
+  genes_use = NULL,
+  genes_fit = NULL,
+  s.use = 20,
+  do.print = FALSE,
+  gram = TRUE,
+  dcorgene = NULL
+) {
+  
+  ifelse(is.null(genes_use),genes_use<- (rownames(object)),genes_use)
+  ifelse(is.null(genes_fit),genes_fit<-rownames(object),genes_fit)
+  ifelse(is.null(dcorgene),dcorgene<- min(100,floor(length(genes_use)/2) ) ,dcorgene)
+  
+  tsip<-object[genes_fit,]
+  tsg<- object[genes_use,]
+  
+  corgene<- sapply(
+    data.frame(t(tsip)), FUN = function(x,x2){
+      
+      genecorre <- apply( x2[which( x!=0),], 2, FUN = function(x,b){
+        ifelse(length(unique(x)==1) ,x[1]<-x[1]+1e-5 ,x )
+        ifelse(length(unique(b)==1) ,b[1]<-b[1]+1e-5 ,b )
+        cor(x,b)
+      },  x[which( x!=0)])
+      
+      genes_use[order(-genecorre)[1:(dcorgene)]]
+      
+    },t(tsg)
+  )
+  
+  corgene<- rbind(colnames(corgene),corgene)
+ 
+  lasso_fits<- sapply(data.frame(corgene), FUN = function(x,tsip,tsg){
+    
+    x<-as.character(x)
+    cell_loc <- which(tsip[x[1],]==0)
+    
+    lasso_input <- tsg[x[-1] , which(tsip[ x[1], ]!=0) ]
+    lasso_fituse <- tsip[x[1] , which(tsip[ x[1], ]!=0) ]
+    gene_loc <- match(rownames(lasso_input),rownames(tsg))
+
+    lasso_fit<- sapply(data.frame(tsg[ ,cell_loc] ),FUN = function(x,lasso_input,lasso_fituse,gene_loc){
+      lasso_out <- x[gene_loc]
+      ##if genes x[-1] in tsg are unique_length==1,then fit<-0
+      
+      ifelse((length(unique(lasso_out))==1)|( length(unique(lasso_input)) == length( lasso_fituse ) ) , 0 , (lasso.fxn(
+        lasso.input = lasso_input,
+        lasso.output = lasso_out,
+        lasso.fituse = lasso_fituse,
+        s.use = s.use,
+        do.print = do.print,
+        gram = gram
+      ))  )
+      
+    }, lasso_input,lasso_fituse,gene_loc)
+    
+  }, tsip,tsg)
+  
+  
+  imputedobj<- object
+  for (i in 1:length(lasso_fits)){
+    imputedobj[names(lasso_fits)[i], which(imputedobj[names(lasso_fits)[i] ,] ==0)] <- lasso_fits[[i]]
+  }
+  return(imputedobj)
+}
+
+lasso.fxn <- function(
+  lasso.input,
+  lasso.output,
+  lasso.fituse,
+  s.use = 20,
+  gene.name = NULL,
+  do.print = FALSE,
+  gram = TRUE
+) {
+  lasso.model <- lars(
+    x = lasso.input,
+    y = as.numeric(x = lasso.output),
+    type = "lasso",
+    max.steps = s.use * 2,
+    use.Gram = gram
+  )
+  #lasso.fits=predict.lars(lasso.model,lasso.input,type="fit",s=min(s.use,max(lasso.model$df)))$fit
+  lasso.coef <- coef.lars(
+    object = lasso.model
+  )[length(lasso.model$df),]
+  
+  lasso.fits<- lasso.fituse %*% (as.matrix(lasso.coef))
+  #t(as.matrix(lasso.fituse[2,])) %*% ((lasso.coef))
+  
+  return(lasso.fits)
+}
+
